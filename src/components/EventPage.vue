cba90f27ed6f950de99eda0e494460789cbc69f4
sandy <a68b58@gmail.com>
2021-01-23 16:55:48 +0800

    font setting

--- src/components/EventPage.vue
+++ src/components/EventPage.vue
@@ -1,5 +1,11 @@
 <template>
     <div>
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@300&display=swap" rel="stylesheet">
+
+        </head>
         <loading :active.sync="isLoading"></loading>
         <header id="event_page">
             <div class="container-fluid" id="event_page_container">
--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-      
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@300&display=swap" rel="stylesheet">
+
+        </head>
 
     
 
